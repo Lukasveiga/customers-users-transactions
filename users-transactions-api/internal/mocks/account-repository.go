8bcf7ab5c47dd964e3328c5790fcabcf10bbb25f
Lukas Veiga <lukas.veiga@hotmail.com>
2024-07-11 12:48:44 -0300

    feat: add create account usecase unit tests

--- users-transactions-api/internal/mocks/account-repository.go
+++ users-transactions-api/internal/mocks/account-repository.go
@@ -12,7 +12,12 @@ type MockAccountRepository struct {
 func (mock *MockAccountRepository) Create(account *domain.Account) (*domain.Account, error) {
 	args := mock.Called()
 	result := args.Get(0)
-	return result.(*domain.Account), args.Error(1)
+
+	if result != nil {
+		return result.(*domain.Account), args.Error(1)
+	}
+	
+	return nil, args.Error(1)
 }
 
 func (mock *MockAccountRepository) FindAll(tenantId int32) ([]domain.Account, error) {
@@ -21,10 +26,26 @@ func (mock *MockAccountRepository) FindAll(tenantId int32) ([]domain.Account, er
 	return result.([]domain.Account), args.Error(1)
 }
 
+func (mock *MockAccountRepository) FindById(tenantId int32, id int32) (*domain.Account, error) {
+	args := mock.Called()
+	result := args.Get(0)
+
+	if result != nil {
+		return result.(*domain.Account), args.Error(1)
+	}
+	
+	return nil, args.Error(1)
+}
+
 func (mock *MockAccountRepository) FindByNumber(tenantId int32, number string) (*domain.Account, error) {
 	args := mock.Called()
 	result := args.Get(0)
-	return result.(*domain.Account), args.Error(1)
+
+	if result != nil {
+		return result.(*domain.Account), args.Error(1)
+	}
+	
+	return nil, args.Error(1)
 }
 
 func (mock *MockAccountRepository) Update(id int32, account *domain.Account) (*domain.Account, error) {
--- users-transactions-api/internal/usecases/account/create-account_test.go
+++ users-transactions-api/internal/usecases/account/create-account_test.go
@@ -1,29 +1,44 @@
 package usecases
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/Lukasveiga/customers-users-Transaction/config"
 	"github.com/Lukasveiga/customers-users-Transaction/internal/domain"
 	"github.com/Lukasveiga/customers-users-Transaction/internal/mocks"
 	"github.com/Lukasveiga/customers-users-Transaction/internal/shared"
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 )
-
-
 
 func TestCreateAccountUsecase(t *testing.T) {
 	t.Parallel()
 
 	mockRepo := new(mocks.MockAccountRepository)
+	account := &domain.Account{
+		Id: 1,
+		TenantId: 1,
+		Number: uuid.New().String(),
+		Status: "active",
+	}
 	sut := NewCreateAccountUsecase(mockRepo, *config.NewLogger("test"))
 
-	t.Run("Error account number already exists", func(t *testing.T) {
-		account := &domain.Account{
-			Number: "123",
-		}
+	t.Run("Error to find accout", func(t *testing.T) {
+		expectedErr := errors.New("internal repo error")
 
+		mockRepo.On("FindByNumber").Return(nil, expectedErr)
+		defer mockRepo.On("FindByNumber").Unset() 
+
+		result, err := sut.Exec(account)
+
+		assert.Nil(t, result)
+		assert.Equal(t, expectedErr.Error(), err.Error())
+	})
+
+	t.Run("Error account number already exists", func(t *testing.T) {
 		mockRepo.On("FindByNumber").Return(account, nil)
+		defer mockRepo.On("FindByNumber").Unset()
 
 		expectedErr := &shared.AlreadyExistsError{
 			Object: "account",
@@ -33,6 +48,34 @@ func TestCreateAccountUsecase(t *testing.T) {
 		result, err := sut.Exec(account)
 
 		assert.Nil(t, result)
-		assert.Equal(t, err.Error(), expectedErr.Error())
+		assert.Equal(t, expectedErr.Error(), err.Error())
+	})
+
+	t.Run("Error to create account", func(t *testing.T) {
+		expectedErr := errors.New("internal repo error")
+
+		mockRepo.On("FindByNumber").Return(nil, nil)
+		defer mockRepo.On("FindByNumber").Unset()
+
+		mockRepo.On("Create").Return(nil, expectedErr)
+		defer mockRepo.On("Create").Unset()
+
+		result, err := sut.Exec(account)
+
+		assert.Nil(t, result)
+		assert.Equal(t, expectedErr.Error(), err.Error())
+	})
+
+	t.Run("Success create new account", func(t *testing.T) {
+		mockRepo.On("FindByNumber").Return(nil, nil)
+		defer mockRepo.On("FindByNumber").Unset()
+
+		mockRepo.On("Create").Return(account, nil)
+		defer mockRepo.On("Create").Unset()
+
+		result, err := sut.Exec(account)
+
+		assert.Nil(t, err)
+		assert.Equal(t, account, result)
 	})
 }
