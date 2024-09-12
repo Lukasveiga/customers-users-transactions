0d368ab4a95bdeb5c1c01744d66269dd1e65472d
Lukas Veiga <lukas.veiga@hotmail.com>
2024-09-12 10:37:32 -0300

    fix: lint

--- users-transactions-api/internal/usecases/grpc/transaction-info.go
+++ users-transactions-api/internal/usecases/grpc/transaction-info.go
@@ -47,7 +47,7 @@ func (ti *TransactionInfo) SearchTransactionInfo(req *genproto.SearchTransaction
 
 	if err != nil {
 		if err == sql.ErrNoRows {
-			return status.Errorf(codes.NotFound, fmt.Sprintf("account with id %d not found", filter.GetAccountId()))
+			return status.Errorf(codes.NotFound, fmt.Sprintf("account with id %d not found", filter.GetAccountId()), err)
 		}
 
 		slog.Error(
